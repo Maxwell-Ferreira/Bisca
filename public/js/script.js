311715bf6483fc55e9365a8457b4c6b9f789a53c
Maxwell-Ferreira <ferreiramaxwell531@gmail.com>
2021-04-07 00:01:16 -0300

    fixed link socketio client

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
-//var socket = io("https://biscabraba.herokuapp.com");
-var socket = io("http://localhost:3000");
+var socket = io("https://biscabraba.herokuapp.com");
 
 var id = "";
 var idSala = "";
